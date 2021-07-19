61f88b087fd0a71cb5c0d6bba1e12c62c4d26a2f
alireza <alireza.lm69@gmail.com>
2021-07-19 00:45:41 -0300

    fetch all implemented

--- src/DAL/managers.py
+++ src/DAL/managers.py
@@ -13,6 +13,10 @@ class IManager(ABC):
     def insert(self, obj: BaseModel):
         pass
 
+    @abstractmethod
+    def list(self):
+        pass
+
 
 class MemberManager(IManager):
 
@@ -21,7 +25,7 @@ class MemberManager(IManager):
         try:
             cursor = connection.cursor()
 
-            sql = """INSERT INTO public."Members" (mem_id, mem_name, mem_family, address, phone, age, mem_date)""" +\
+            sql = """INSERT INTO public."Members" (mem_id, mem_name, mem_family, address, phone, age, mem_date)""" + \
                   """VALUES (%s, %s, %s, %s, %s, %s, %s);"""
 
             cursor.execute(
@@ -38,8 +42,28 @@ class MemberManager(IManager):
             )
             connection.commit()
             cursor.close()
-            connection.close()
             click.echo("new Member inserted")
             return 1
         except (Exception, psycopg2.Error) as error:
             print(error)
+        finally:
+            connection.close()
+
+    def list(self):
+        connection = DBConnector().get_connection()
+        try:
+            cursor = connection.cursor()
+            sql = """SELECT * FROM public."Members" """
+
+            cursor.execute(sql)
+            click.echo(f"number of items: {cursor.rowcount}")
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+
+            cursor.close()
+
+        except (Exception, psycopg2.Error) as error:
+            click.echo(error)
+        finally:
+            connection.close()
